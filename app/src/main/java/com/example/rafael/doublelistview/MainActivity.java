a807c3ade6f8dd75d8c810d4b3677e0feead28ba
Chuque <Rafa_chuque@hotmail.com>
2018-04-20 02:16:23 -0300

    Criado custom adapter para a listview

--- app/src/main/java/com/example/rafael/doublelistview/MainActivity.java
+++ app/src/main/java/com/example/rafael/doublelistview/MainActivity.java
@@ -22,19 +22,19 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<SubItem> subItems = new ArrayList<>();
         for(int i=0;i<2;i++){
             SubItem subItem = new SubItem();
-            subItem.setText("abc");
+            subItem.setText("SubItem " + i);
             subItems.add(subItem);
         }
 
         for(int i=0;i<3;i++) {
             Item item = new Item();
-            item.setText("123");
+            item.setText("Item " + i);
             item.setSubItems(subItems);
             items.add(item);
         }
 
         ListView itemListView = findViewById(R.id.itemList);
-        ArrayAdapter<Item> itemAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.itemText, items);
+        ItemAdapter itemAdapter = new ItemAdapter(items, this);
         itemListView.setAdapter(itemAdapter);
     }
 }
